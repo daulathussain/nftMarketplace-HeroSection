585b8b97997ff3f020f0a78b243272dcb094a7e1
daulathussain <daulathussain92@gmail.com>
2022-08-22 11:06:04 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//INTERNAL IMPORT
+import Style from "../styles/index.module.css";
+import { HeroSection } from "../components/componentsindex";
+
+const Home = () => {
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+    </div>
+  );
+};
+
+export default Home;
